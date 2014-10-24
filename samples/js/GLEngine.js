0afcef48abe3cfaa62df4030cdfc4b2a5a962c7c
Kei Takahashi <kei.takahashi@dena.com>
2014-10-24 19:51:12 +0900

    Update

--- samples/js/GLEngine.js
+++ samples/js/GLEngine.js
@@ -1,96 +1,152 @@
 /// <reference path="./d.ts/gl-matrix/gl-matrix.d.ts" />
-var GLEngine = (function () {
-    function GLEngine(canvasId, width, height) {
-        this.width = width;
-        this.height = height;
-        this.canvas = document.getElementById(canvasId);
-        try {
-            this.gl = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
+var GL;
+(function (GL) {
+    var requestAnimFrame = (function () {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+            window.setTimeout(callback, 1000 / 60, new Date().getTime());
+        };
+    })();
+    var Engine = (function () {
+        function Engine(canvasId, width, height) {
+            this.width = width;
+            this.height = height;
+            this.canvas = document.getElementById(canvasId);
+            try {
+                this.gl = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
+            }
+            catch (e) {
+                console.error(e);
+            }
+            this.reset();
         }
-        catch (e) {
-            console.error(e);
-        }
-        this.initialize();
-    }
-    GLEngine.prototype.initialize = function () {
-        var gl = this.gl;
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clearDepth(1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    };
-    GLEngine.prototype.bindNewVBO = function (vertices) {
-        var gl = this.gl;
-        var buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        return buffer;
-    };
-    GLEngine.prototype.bindNewIBO = function (vertices) {
-        var gl = this.gl;
-        var buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array, gl.STATIC_DRAW);
-        return buffer;
-    };
-    GLEngine.prototype.createVBO = function (vertices) {
-        var gl = this.gl;
-        var buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        return buffer;
-    };
-    GLEngine.prototype.setAttribute = function (program, name, stride) {
-        var gl = this.gl;
-        var location = gl.getAttribLocation(program, name);
-        gl.enableVertexAttribArray(location);
-        gl.vertexAttribPointer(location, stride, gl.FLOAT, false, 0, 0);
-        return location;
-    };
-    GLEngine.prototype.setProgramWithShaders = function () {
-        var shaders = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            shaders[_i - 0] = arguments[_i];
-        }
-        var gl = this.gl;
-        var program = gl.createProgram();
-        for (var i = 0, iz = shaders.length; i < iz; i++) {
-            gl.attachShader(program, shaders[i]);
-        }
-        gl.linkProgram(program);
-        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            gl.useProgram(program);
-            return program;
-        }
-        else {
-            console.error(gl.getProgramInfoLog(program));
-        }
-    };
-    GLEngine.prototype.getShader = function (shaderTargetId) {
-        var gl = this.gl;
-        var shaderScript = document.getElementById(shaderTargetId);
-        var source, shader;
-        if (!shaderScript) {
-            console.warn('shader script not found');
-            return;
-        }
-        source = shaderScript.textContent;
-        switch (shaderScript.type) {
-            case 'x-shader/x-fragment':
-                shader = gl.createShader(gl.FRAGMENT_SHADER);
-                break;
-            case 'x-shader/x-vertex':
-                shader = gl.createShader(gl.VERTEX_SHADER);
-                break;
-            default:
+        Engine.prototype.reset = function () {
+            var gl = this.gl;
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            gl.clearDepth(1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        };
+        Engine.prototype.bindNewVBO = function (vertices) {
+            var gl = this.gl;
+            var buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            return buffer;
+        };
+        Engine.prototype.bindNewIBO = function (vertices) {
+            var gl = this.gl;
+            var buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(vertices), gl.STATIC_DRAW);
+            return buffer;
+        };
+        Engine.prototype.createVBO = function (vertices) {
+            var gl = this.gl;
+            var buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
+            return buffer;
+        };
+        Engine.prototype.setAttribute = function (program, name, stride) {
+            var gl = this.gl;
+            var location = gl.getAttribLocation(program, name);
+            gl.enableVertexAttribArray(location);
+            gl.vertexAttribPointer(location, stride, gl.FLOAT, false, 0, 0);
+            return location;
+        };
+        Engine.prototype.setProgramWithShaders = function () {
+            var shaders = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                shaders[_i - 0] = arguments[_i];
+            }
+            var gl = this.gl;
+            var program = gl.createProgram();
+            for (var i = 0, iz = shaders.length; i < iz; i++) {
+                gl.attachShader(program, shaders[i]);
+            }
+            gl.linkProgram(program);
+            if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+                gl.useProgram(program);
+                return program;
+            }
+            else {
+                console.error(gl.getProgramInfoLog(program));
+            }
+        };
+        Engine.prototype.getShader = function (shaderTargetId) {
+            var gl = this.gl;
+            var shaderScript = document.getElementById(shaderTargetId);
+            var source, shader;
+            if (!shaderScript) {
+                console.warn('shader script not found');
                 return;
+            }
+            source = shaderScript.textContent;
+            switch (shaderScript.type) {
+                case 'x-shader/x-fragment':
+                    shader = gl.createShader(gl.FRAGMENT_SHADER);
+                    break;
+                case 'x-shader/x-vertex':
+                    shader = gl.createShader(gl.VERTEX_SHADER);
+                    break;
+                default:
+                    return;
+            }
+            gl.shaderSource(shader, source);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+            }
+            return shader;
+        };
+        return Engine;
+    })();
+    GL.Engine = Engine;
+    var Animation = (function () {
+        function Animation(callback) {
+            this.isAnimated = false;
+            this.listeners = [];
+            this.now = Date.now;
+            if (callback) {
+                this.addListener(callback);
+            }
         }
-        gl.shaderSource(shader, source);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-        }
-        return shader;
-    };
-    return GLEngine;
-})();
+        Animation.prototype.play = function () {
+            this.isAnimated = true;
+            this.startedAt = this.now();
+            this._exec();
+        };
+        Animation.prototype.stop = function () {
+            this.startedAt = null;
+            this.isAnimated = false;
+        };
+        Animation.prototype.addListener = function (callback) {
+            this.listeners.push(callback);
+        };
+        Animation.prototype.removeListener = function (callback) {
+            var listeners = this.listeners;
+            var index = listeners.indexOf(callback);
+            if (index > -1) {
+                listeners.splice(index, 1);
+            }
+        };
+        Animation.prototype._exec = function () {
+            var _this = this;
+            if (!this.isAnimated) {
+                return;
+            }
+            var listeners = this.listeners;
+            var startedAt = this.startedAt;
+            var now = this.now();
+            var duration = now - this.startedAt;
+            for (var i = 0, callback; callback = listeners[i]; i++) {
+                callback(duration, now, startedAt);
+            }
+            requestAnimationFrame(function () {
+                _this._exec();
+            });
+        };
+        return Animation;
+    })();
+    GL.Animation = Animation;
+})(GL || (GL = {}));
--- samples/ts/GLEngine.ts
+++ samples/ts/GLEngine.ts
@@ -1,106 +1,171 @@
 /// <reference path="./d.ts/gl-matrix/gl-matrix.d.ts" />
 
-class GLEngine {
-    private canvas:HTMLCanvasElement;
-    private gl:WebGLRenderingContext;
+module GL {
 
-    constructor(canvasId:string, private width:number, private height:number) {
-        this.canvas = <HTMLCanvasElement>document.getElementById(canvasId);
+    var requestAnimFrame: (callback:() => void) => void = (function () {
+        return window.requestAnimationFrame ||
+            (<any>window).webkitRequestAnimationFrame ||
+            (<any>window).mozRequestAnimationFrame ||
+            (<any>window).oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60, new Date().getTime());
+            };
+    })();
 
-        try {
-            this.gl = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
-        } catch (e) {
-            console.error(e);
+    export class Engine {
+        private canvas:HTMLCanvasElement;
+        private gl:WebGLRenderingContext;
+
+        constructor(canvasId:string, private width:number, private height:number) {
+            this.canvas = <HTMLCanvasElement>document.getElementById(canvasId);
+
+            try {
+                this.gl = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
+            } catch (e) {
+                console.error(e);
+            }
+            this.reset();
         }
-        this.initialize();
-    }
 
-    initialize() {
-        var gl = this.gl;
+        reset() {
+            var gl = this.gl;
 
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clearDepth(1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    }
-
-    bindNewVBO(vertices:number[]): WebGLBuffer {
-        var gl = this.gl;
-        var buffer:WebGLBuffer = gl.createBuffer();
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        return buffer;
-    }
-
-    bindNewIBO(vertices:number[]): WebGLBuffer {
-        var gl = this.gl;
-        var buffer:WebGLBuffer = gl.createBuffer();
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array, gl.STATIC_DRAW);
-        return buffer;
-    }
-
-    createVBO(vertices:number[]):WebGLBuffer {
-        var gl = this.gl;
-        var buffer:WebGLBuffer = gl.createBuffer();
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-        return buffer;
-    }
-
-    setAttribute(program:WebGLProgram, name:string, stride:number):number {
-        var gl = this.gl;
-        var location = gl.getAttribLocation(program, name);
-
-        gl.enableVertexAttribArray(location);
-        gl.vertexAttribPointer(location, stride, gl.FLOAT, false, 0, 0);
-        return location;
-    }
-
-    setProgramWithShaders(...shaders: WebGLShader[]):WebGLProgram {
-        var gl = this.gl;
-        var program = gl.createProgram();
-
-        for (var i = 0, iz = shaders.length; i < iz; i++) {
-            gl.attachShader(program, shaders[i]);
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            gl.clearDepth(1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         }
-        gl.linkProgram(program);
-        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            gl.useProgram(program);
-            return program;
-        } else {
-            console.error(gl.getProgramInfoLog(program));
-        }
-    }
 
-    getShader(shaderTargetId:string):WebGLShader {
-        var gl = this.gl;
-        var shaderScript = <HTMLScriptElement>document.getElementById(shaderTargetId);
-        var source:string, shader:WebGLShader;
+        bindNewVBO(vertices:number[]):WebGLBuffer {
+            var gl = this.gl;
+            var buffer:WebGLBuffer = gl.createBuffer();
 
-        if (!shaderScript) {
-            console.warn('shader script not found');
-            return;
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            return buffer;
         }
-        source = shaderScript.textContent;
-        switch (shaderScript.type) {
-            case 'x-shader/x-fragment':
-                shader = gl.createShader(gl.FRAGMENT_SHADER);
-                break;
-            case 'x-shader/x-vertex':
-                shader = gl.createShader(gl.VERTEX_SHADER);
-                break;
-            default:
+
+        bindNewIBO(vertices:number[]):WebGLBuffer {
+            var gl = this.gl;
+            var buffer:WebGLBuffer = gl.createBuffer();
+
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(vertices), gl.STATIC_DRAW);
+            return buffer;
+        }
+
+        createVBO(vertices:number[]):WebGLBuffer {
+            var gl = this.gl;
+            var buffer:WebGLBuffer = gl.createBuffer();
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
+            return buffer;
+        }
+
+        setAttribute(program:WebGLProgram, name:string, stride:number):number {
+            var gl = this.gl;
+            var location = gl.getAttribLocation(program, name);
+
+            gl.enableVertexAttribArray(location);
+            gl.vertexAttribPointer(location, stride, gl.FLOAT, false, 0, 0);
+            return location;
+        }
+
+        setProgramWithShaders(...shaders:WebGLShader[]):WebGLProgram {
+            var gl = this.gl;
+            var program = gl.createProgram();
+
+            for (var i = 0, iz = shaders.length; i < iz; i++) {
+                gl.attachShader(program, shaders[i]);
+            }
+            gl.linkProgram(program);
+            if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+                gl.useProgram(program);
+                return program;
+            } else {
+                console.error(gl.getProgramInfoLog(program));
+            }
+        }
+
+        getShader(shaderTargetId:string):WebGLShader {
+            var gl = this.gl;
+            var shaderScript = <HTMLScriptElement>document.getElementById(shaderTargetId);
+            var source:string, shader:WebGLShader;
+
+            if (!shaderScript) {
+                console.warn('shader script not found');
                 return;
+            }
+            source = shaderScript.textContent;
+            switch (shaderScript.type) {
+                case 'x-shader/x-fragment':
+                    shader = gl.createShader(gl.FRAGMENT_SHADER);
+                    break;
+                case 'x-shader/x-vertex':
+                    shader = gl.createShader(gl.VERTEX_SHADER);
+                    break;
+                default:
+                    return;
+            }
+            gl.shaderSource(shader, source);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+            }
+            return shader;
         }
-        gl.shaderSource(shader, source);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-        }
-        return shader;
     }
+
+    interface IAnimationCallback {
+        (duration:number, currentTime:number, startTime:number): void;
+    }
+
+    export class Animation {
+        private startedAt:number;
+        private isAnimated:boolean = false;
+        private listeners:IAnimationCallback[] = [];
+        private now:Function = Date.now;
+        constructor(callback?:IAnimationCallback) {
+            if (callback) {
+                this.addListener(callback);
+            }
+        }
+        play() {
+            this.isAnimated = true;
+            this.startedAt = this.now();
+            this._exec();
+        }
+        stop() {
+            this.startedAt = null;
+            this.isAnimated = false;
+        }
+        addListener(callback:IAnimationCallback) {
+            this.listeners.push(callback);
+        }
+        removeListener(callback:IAnimationCallback) {
+            var listeners = this.listeners;
+            var index = listeners.indexOf(callback);
+
+            if (index > -1) {
+                listeners.splice(index, 1);
+            }
+        }
+        private _exec() {
+            if (!this.isAnimated) {
+                return;
+            }
+            var listeners = this.listeners;
+            var startedAt = this.startedAt;
+            var now = this.now();
+            var duration = now - this.startedAt;
+
+            for (var i = 0, callback; callback = listeners[i]; i++) {
+                callback(duration, now, startedAt);
+            }
+            requestAnimationFrame(() => { this._exec(); });
+        }
+    }
+
 }
